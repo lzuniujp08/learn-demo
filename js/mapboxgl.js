f3308c40716aed05f6980da51c0ace875c8b1de7
niujp08@qq.com <niujp08@qq.com>
2020-03-17 19:18:34 +0800

    mapbox gl

--- js/mapboxgl.js
+++ js/mapboxgl.js
@@ -14,6 +14,12 @@ var app = new Vue({
       const token = 'pk.eyJ1IjoibHp1bml1anAwOCIsImEiOiJjam5iZmZwamMwN3RnM2twOHByeHJ4NzJoIn0.IVVIcCdRmFQE8-nQF-8wTA';
       mapboxgl.accessToken = token;
       var url = 'http://10.30.17.59:8000///public/gdfs/meter/20200317/GDFS_meter_202003161200_202003171200_TEM.png';
+      var coords = [
+        [70, 65],
+        [145, 65],
+        [145, 15],
+        [70, 15]
+      ];
       var mapStyle = {
         "version": 8,
         "name": "Dark",
@@ -22,15 +28,10 @@ var app = new Vue({
             "type": "vector",
             "url": "mapbox://mapbox.mapbox-streets-v6"
           },
-          "overlay": {
-            "type": "image",
-            "url": url,
-            "coordinates": [
-              [70, 65],
-              [145, 65],
-              [145, 15],
-              [70, 15]
-            ]
+          "loopimage": {
+            type: 'image',
+            url: url,
+            coordinates: coords
           }
         },
         "sprite": "mapbox://sprites/mapbox/dark-v9",
@@ -43,12 +44,11 @@ var app = new Vue({
             }
           },
           {
-            "id": "water",
-            "source": "mapbox",
-            "source-layer": "water",
-            "type": "fill",
-            "paint": {
-              "fill-color": "#2c2c2c"
+            'id': 'loopimage',
+            'source': 'loopimage',
+            'type': 'raster',
+            'paint': {
+              'raster-opacity': 0.65
             }
           },
           {
@@ -57,17 +57,7 @@ var app = new Vue({
             "source-layer": "admin",
             "type": "line",
             "paint": {
-              "line-color": "#797979",
-              "line-dasharray": [2, 2, 6, 2]
-            },
-            "filter": ["all", ["==", "maritime", 0]]
-          },
-          {
-            "id": "overlay",
-            "source": "overlay",
-            "type": "raster",
-            "paint": {
-              "raster-opacity": 0.65
+              "line-color": "#797979"
             }
           }
         ]
@@ -79,8 +69,15 @@ var app = new Vue({
         minZoom: 0,
         zoom: 5,
         center: [107.5, 40],
-        style: mapStyle
+        style: mapStyle,
+        attributionControl: false
       });
+
+      // var source = map.getSource("loopimage");
+      // source.updateImage({
+      //   url: url,
+      //   coordinates: coords
+      // });
     }
   }
 });
