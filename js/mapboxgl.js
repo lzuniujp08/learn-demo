6c761e887e4839af08041a84e125889148239e47
niujp08@qq.com <niujp08@qq.com>
2020-03-18 16:49:29 +0800

    addmapbox demo

--- js/mapboxgl.js
+++ js/mapboxgl.js
@@ -3,7 +3,13 @@ var that, map;
 var app = new Vue({
   el: '#app',
   data: {
-    vector: null
+    playInterval: 500,
+    playFlag: 0,
+    isplay: false,
+    images: [],
+    current: 0,
+    currentTime: '',
+    markers: []
   },
   mounted: function() {
     that = this;
@@ -13,13 +19,6 @@ var app = new Vue({
     initMap: function() {
       const token = 'pk.eyJ1IjoibHp1bml1anAwOCIsImEiOiJjam5iZmZwamMwN3RnM2twOHByeHJ4NzJoIn0.IVVIcCdRmFQE8-nQF-8wTA';
       mapboxgl.accessToken = token;
-      var url = 'http://10.30.17.59:8000///public/gdfs/meter/20200317/GDFS_meter_202003161200_202003171200_TEM.png';
-      var coords = [
-        [70, 65],
-        [145, 65],
-        [145, 15],
-        [70, 15]
-      ];
       var mapStyle = {
         "version": 8,
         "name": "Dark",
@@ -28,10 +27,10 @@ var app = new Vue({
             "type": "vector",
             "url": "mapbox://mapbox.mapbox-streets-v6"
           },
-          "loopimage": {
-            type: 'image',
-            url: url,
-            coordinates: coords
+          "OSM": {
+            "type": "raster",
+            "tiles": ['https://a.tile.openstreetmap.org/{z}/{x}/{y}.png'],
+            "tileSize": 256,
           }
         },
         "sprite": "mapbox://sprites/mapbox/dark-v9",
@@ -44,12 +43,11 @@ var app = new Vue({
             }
           },
           {
-            'id': 'loopimage',
-            'source': 'loopimage',
-            'type': 'raster',
-            'paint': {
-              'raster-opacity': 0.65
-            }
+            "id": "OSM",
+            "type": "raster",
+            "source": "OSM",
+            "minzoom": 0,
+            "maxzoom": 22
           },
           {
             "id": "boundaries",
@@ -67,17 +65,155 @@ var app = new Vue({
         container: 'map',
         maxZoom: 18,
         minZoom: 0,
-        zoom: 5,
-        center: [107.5, 40],
+        zoom: 3,
+        center: [109.1699, 45.9719],
         style: mapStyle,
         attributionControl: false
       });
+      let firstInit = true;
+      map.on('styledata', function() {
+        if (firstInit) {
+          that.getImageData();
+          firstInit = false;
+          map.loadImage('../css/lightning.png', function(error, image) {
+            if (error) throw error;
+            map.addImage('lightning', image);
+          });
+        }
+      });
+    },
+    getImageData() {
+      $.get('../data/tem.json', res => {
+        that.images = res.data.list;
+        that.play();
 
-      // var source = map.getSource("loopimage");
-      // source.updateImage({
-      //   url: url,
-      //   coordinates: coords
+        that.addMarkers();
+      });
+    },
+    play() {
+      that.currentTime = that.images[that.current].time;
+      var coords = [
+        [70, 65],
+        [145, 65],
+        [145, 15],
+        [70, 15]
+      ];
+      const source = map.getSource('loopimage');
+      if (!source) {
+        map.addSource('loopimage', {
+          type: 'image',
+          url: that.images[that.current].url,
+          coordinates: coords
+        });
+        map.addLayer({
+          'id': 'loopimage',
+          'source': 'loopimage',
+          'type': 'raster',
+          'paint': {
+            'raster-opacity': 0.65,
+            'raster-fade-duration': 0
+          }
+        });
+      } else {
+        source.updateImage({
+          url: that.images[that.current].url,
+          coordinates: coords
+        })
+      }
+    },
+    stopPlay() {
+      window.clearInterval(that.playFlag);
+    },
+    togglePlay() {
+      if (that.isplay) {
+        that.stopPlay();
+      } else {
+        that.playFlag = setInterval(function() {
+          that.current++;
+          that.play();
+          if (that.current === that.images.length) {
+            that.stopPlay();
+          }
+        }, that.playInterval);
+      }
+      that.isplay = !that.isplay;
+    },
+    addMarkers() {
+      $.get('../data/capital.json', res => {
+        var geojson = {
+          'type': 'FeatureCollection',
+          'features': []
+        };
+        for (var i = 0; i < res.length; i++) {
+          const r = res[i];
+          const ele = document.createElement('div');
+          ele.setAttribute('class', 'map-label');
+          ele.innerHTML = r.name;
+          const option = {
+            element: ele,
+            anchor: 'bottom',
+            offset: [0, -10]
+          }
+          // var marker = new mapboxgl.Marker(option).setLngLat([r.lon, r.lat]).addTo(map);
+          // that.markers.push(marker);
+
+          geojson.features.push({
+            type: 'Feature',
+            geometry: {
+              type: 'Point',
+              coordinates: [r.lon, r.lat],
+              properties: {
+                name: r.name
+              }
+            }
+          });
+        }
+        that.addGeojson(geojson);
+      })
+    },
+    toggleMarkers() {
+      if (that.markers.length > 0) {
+        const layer = map.getLayer('points');
+        if(layer) {
+          map.removeLayer('points');
+          map.removeSource('points');
+        }
+        for (var i = 0; i < that.markers.length; i++) {
+          var marker = that.markers[i];
+          marker.remove();
+        }
+        that.markers = [];
+      } else {
+        that.addMarkers();
+      }
+    },
+    addGeojson(geojson) {
+      map.addSource('points', {
+        type: 'geojson',
+        data: geojson
+      });
+      // map.addLayer({
+      //   id: 'points',
+      //   type: 'circle',
+      //   source: 'points',
+      //   paint: {
+      //     'circle-color': '#ff0000',
+      //     'circle-radius': 3,
+      //     'circle-stroke-width': 0
+      //   }
       // });
+      map.addLayer({
+        'id': 'points',
+        'type': 'symbol',
+        'source': 'points',
+        'layout': {
+          'icon-image': 'lightning',
+          'icon-size': 0.45,
+          'text-field': ['get', 'name'],
+          'text-offset': [0, 0],
+          'text-anchor': 'bottom'
+        }
+      });
     }
   }
 });
